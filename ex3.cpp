6a2e9fa015cd097ced0066bab2e516a356caea83
Nguye <75427876+nguyenphu-3204@users.noreply.github.com>
2023-01-02 09:50:17 +0700

    Update ex3.cpp
    Nhập vào một chuỗi cần tìm kiếm. In ra màn hình, tên các học sinh chứa chuỗi đó. (Không phân biệt chữ hoa chữ thường)

--- ex3.cpp
+++ ex3.cpp
@@ -1,6 +1,8 @@
 /*
 Ở phiên bản này mình dùng get_name() - hàm tự viết thay cho fs.getline()
 để tường minh hơn, tuy nhiên ở những bài tập sau mình sẽ dùng hàm có sẵn.
+Thứ hai, trong biểu diễn chuỗi ta có thể dùng mảng char và kiểu dữ liệu string
+NHƯNG fs.getline() không hỗ trợ string.c_str() !!!!
 */
 #include<bits/stdc++.h>
 #define for_(i, b, e) for(int i=b; i<=e; i++)
